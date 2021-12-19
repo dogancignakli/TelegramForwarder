21626a9d36e63315a39e131e3ec15e37690ed0da
Doğan <dogancignakli@gmail.com>
2021-12-19 09:11:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,23 @@
 from telethon import TelegramClient,events
 
-api_id = 3932493
-api_hash = '04c9d06a0d44124fec4213db770fb7f7'
+api_id = "Your Api Id Here"
+api_hash = "Your api hash here"
 client = TelegramClient('DoganSlx', api_id, api_hash)
-coinlegs = 882632305
-yevmiyeciler = 'https://t.me/joinchat/IudzGql11hEwDNqm'
+fromChat = 111111111 # Chat from which bot will read data
+toChat = 'The chat that you want to forward messages'
 
-@client.on(events.NewMessage(chats=coinlegs))
+@client.on(events.NewMessage(chats=fromChat))
 async def event_handler(event):
-    source_channel = await client.get_entity(coinlegs)
-    destination_channel = await client.get_entity(yevmiyeciler)
+    source_channel = await client.get_entity(fromchat)
+    destination_channel = await client.get_entity(toChat)
     messages = await client.get_messages(source_channel,limit=1)
     for x in messages:
         print(x.text)
-    #await client.send_message(destination_channel,message="HelloFromDoganSLX1.6")
     await client.forward_messages(destination_channel,messages)
 
 client.start()
 client.run_until_disconnected()
-    # await client.send_message(destination_channel,message="HelloFromDoganSLX1.6")
-    # await client.forward_messages(destination_channel,messages)
-
-#     global chat
-#     global sender
-#     global chat_id
-#     global sender_id
-#     chat = await event.get_chat()
-#     sender = await event.get_sender()
-#     chat_id = event.chat_id
-#     sender_id = event.sender_id
-
-
-
 async def main():
     me = await client.get_me()
-    #print(me.stringify())
-
-    # async for dialog in client.iter_dialogs():
-    #     print(dialog.name,'has ID',dialog.id)
-   # await client.send_message(1001364508253,'Telethon Python API Test')
-   # source_channel  = await client.get_entity(sender)
-    #destination_channel = await client.get_entity('https://t.me/joinchat/IudzGql11hEwDNqm')
-    # messages = await client.get_messages(source_channel,limit=1)
-    # for x in messages:
-    #     print(x.text)
-   # await client.send_message(destination_channel,message="HelloFromDoganSLX1.6")
-   # await client.forward_messages(destination_channel,messages)
-
-
-
+   
 
